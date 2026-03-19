5b827f719f242e0393c331a9adb276f2b75fc5e9
Jorge Alejandro Rubio Giraldo <jorge.rubio@est.iudigital.edu.co>
2026-03-19 18:09:00 -0500

    Se agregan comentarios explicativos a la clase Producto

--- Producto.java
+++ Producto.java
@@ -1,9 +1,11 @@
 public class Producto {
+    // Atributos del nodo
     int id;
     String nombre;
     Producto izquierdo;
     Producto derecho;
 
+    // Punteros para las ramas del árbol
     public Producto(int id, String nombre) {
         this.id = id;
         this.nombre = nombre;
